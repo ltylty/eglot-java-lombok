79d52e652daf4046085d889186534d1674c51e90
ltylty <1137588860@qq.com>
2024-05-15 21:14:59 +0800

    Create eglot-java-lombok.el

---
+++ eglot-java-lombok.el
@@ -0,0 +1,54 @@
+(require 'eglot)
+
+(defvar eglot-java-lombok/enabled nil
+  "Indicates the LSP server should be started with Lombok.")
+
+(defvar eglot-java-lombok/version nil
+  "When non-nil, use the specified Lombok version, otherwise use the latest.")
+
+(defvar eglot-java-lombok/jar-url-base "https://projectlombok.org/downloads/"
+  "The base path to download Lombok jars from.")
+
+(defvar eglot-java-lombok/dir user-emacs-directory
+  "The path on disk where lombok jars are saved.")
+
+(defun eglot-java-lombok/jar-file ()
+  "Get the filename for the Lombok jar."
+  (concat "lombok"
+          (when eglot-java-lombok/version "-")
+          eglot-java-lombok/version
+          ".jar"))
+
+(defun eglot-java-lombok/jar-path ()
+  "Generate the path on disk for the Lombok jar."
+  (concat user-emacs-directory (eglot-java-lombok/jar-file)))
+
+(defun eglot-java-lombok/download-jar ()
+  "Download the latest lombok jar for use with LSP."
+  (let* ((lombok-url (url-generic-parse-url eglot-java-lombok/jar-url-base))
+         (base-path (file-name-as-directory (url-filename lombok-url)))
+         (file-path (concat base-path (eglot-java-lombok/jar-file))))
+    (setf (url-filename lombok-url) file-path)
+    (url-copy-file lombok-url (eglot-java-lombok/jar-path))))
+
+(defun eglot-java-lombok/append-vmargs ()
+  "Apply lombok args."
+  (let ((jdtls-arguments `("jdtls"
+			    "-XX:+UseG1GC"
+			    ,(concat "--jvm-arg=-javaagent:" (eglot-java-lombok/jar-path)))))
+      (add-to-list 'eglot-server-programs `(java-mode . ,jdtls-arguments))
+      (add-to-list 'eglot-server-programs `(java-ts-mode . ,jdtls-arguments))))
+
+(defun eglot-java-lombok/setup ()
+  "Download Lombok if it hasn't been downloaded already."
+  (when (not (file-exists-p (eglot-java-lombok/jar-path)))
+    (message "Could not find lombok for lsp-java.  Downloading...")
+    (eglot-java-lombok/download-jar)))
+
+(defun eglot-java-lombok/init ()
+  "Initialize eglot-java-lombok."
+  (when eglot-java-lombok/enabled
+    (eglot-java-lombok/setup)
+    (eglot-java-lombok/append-vmargs)))
+
+(provide 'eglot-java-lombok)
